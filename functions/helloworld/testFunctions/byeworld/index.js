0c1fc4311f099618119fc001ee18fc410bbd8439
wwon866 <wwon866@aucklanduni.ac.nz>
2023-05-18 03:30:59 +1200

    reverted byeworld/index

--- functions/helloworld/testFunctions/byeworld/index.js
+++ functions/helloworld/testFunctions/byeworld/index.js
@@ -16,32 +16,25 @@
 
 // [START functions_helloworld_get]
 const functions = require('@google-cloud/functions-framework');
-const { request } = require('http');
+const http = require('http');
 
-const options = new URL('https://weigh9-backend.vercel.app/hardware/register?token=1234');
-// var options = {
-//     url: "https://weigh9-backend.vercel.app/hardware/register?token=1234",
-//     json: true
-//  };
+
+var options = new URL("https://weigh9-backend.vercel.app/hardware/register?token=1234")
+
 // Register an HTTP function with the Functions Framework that will be executed
 // when you make an HTTP request to the deployed function's endpoint.
 functions.http('Byeworld', (req, res) => {
     // res.redirect("https://weigh9-backend.vercel.app/docs")
     // res.send('Bye World!');
-    const req2 = http.request(options, (res) => {
-        console.log(`STATUS: ${res.statusCode}`);
-        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-        res.setEncoding('utf8');
-        res.on('data', (chunk) => {
-          console.log(`BODY: ${chunk}`);
-        });
-        res.on('end', () => {
-          console.log('No more data in response.');
-        });
-      });
+    // const req2 = http.request(options, (res) => {
+        
+    //     res.on('data', (chunk) => {
+    //       res.send(chunk.json())
+    //     });
+    //   });
       
     res.send(
-      req2.chunk  
+      options
     )
 });
 // [END functions_helloworld_get]
